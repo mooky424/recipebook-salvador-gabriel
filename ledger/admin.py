519780681259748e481cbe7d03e24908b359be6e
mooky424 <gabosalvador2004@gmail.com>
2024-03-01 20:10:23 +0800

    lab 2 changes

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from.models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline]
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
